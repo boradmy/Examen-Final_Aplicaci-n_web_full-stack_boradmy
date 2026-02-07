d65a456432c9ac8f417288361900bb0a79099b7c
boris <boradmy@gmail.com>
2026-02-07 12:57:31 -0500

    DJANGLE FINISHIM

--- movies/forms.py
+++ movies/forms.py
@@ -1,14 +1,39 @@
 from django import forms
 from .models import Pelicula, Director
 
-
 class PeliculaForm(forms.ModelForm):
     class Meta:
         model = Pelicula
         fields = '__all__'
-
+        widgets = {
+            'titulo': forms.TextInput(attrs={
+                'class': 'form-control', 
+                'placeholder': 'Ingrese el título de la película'
+            }),
+            'genero': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese el género'
+            }),
+            'anio': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese el año de estreno'
+            }),
+            'director': forms.Select(attrs={'class': 'form-select'}),
+            'picture': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
 
 class DirectorForm(forms.ModelForm):
     class Meta:
         model = Director
         fields = '__all__'
+        widgets = {
+            'nombre': forms.TextInput(attrs={
+                'class': 'form-control', 
+                'placeholder': 'Ingrese el nombre del director'
+            }),
+            'nacionalidad': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingrese la nacionalidad'
+            }),
+            'picture': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
